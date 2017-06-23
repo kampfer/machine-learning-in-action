f26ab4a4be746a3d93e29fdc5bc49972968fc5c3
liaowei02 <liaowei02@baidu.com>
2017-06-23 15:32:51 +0800

    update

--- ch03/trees.py
+++ ch03/trees.py
@@ -42,10 +42,19 @@ def splitDataSet(dataSet, axis, value):
 
 def chooseBestFeatureToSplit(dataSet):
     numFeatures = len(dataSet[0]) - 1
-    baseEnt = calShannonEnt(dataSet)
+    baseEntropy = calShannonEnt(dataSet)
     bestInfoGain = 0.0
     bestFeature = -1
     for i in range(numFeatures):
         featList = [example[i] for example in dataSet]
         uniqueVals = set(featList)
-        print uniqueVals
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
