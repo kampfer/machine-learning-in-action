dd9d35328a7d5ae7e5141fb2f2ca71fa9a6a1364
l.w.kampfer <l.w.kampfer@gmail.com>
2017-06-25 22:22:35 +0800

    决策树

--- ch03/test.py
+++ ch03/test.py
@@ -1,3 +1,12 @@
 import trees
+import treePlotter
 
-myData, labels = trees.createDataSet()
+# myData, labels = trees.createDataSet()
+# myTree = trees.createTree(myData, labels[:])
+# print trees.classify(myTree, labels, [1,0]), trees.classify(myTree, labels, [1,1])
+
+fr = open('lenses.txt')
+lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+lenseLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+lenseTree = trees.createTree(lenses, lenseLabels)
+treePlotter.createPlot(lenseTree)
--- ch03/trees.py
+++ ch03/trees.py
@@ -32,7 +32,7 @@ def createDataSet():
     return dataSet, labels
 
 def splitDataSet(dataSet, axis, value):
-    retDataSet = {}
+    retDataSet = []
     for vec in dataSet:
         if vec[axis] == value:
             reducedVec = vec[:axis]
@@ -52,9 +52,57 @@ def chooseBestFeatureToSplit(dataSet):
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
             prob = len(subDataSet) / float(len(dataSet))
+            # 每个特征的不同特征值的信息熵的加权和，权重是特征值的频率
             newEntropy += prob * calShannonEnt(subDataSet)
         infoGain = baseEntropy - newEntropy
         if (infoGain > bestInfoGain):
             bestInfoGain = infoGain
             bestFeature = i
     return bestFeature
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount:
+            classCount[key] = 0
+        classCount[key] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    del(labels[bestFeat])
+    myTree = {bestFeatLabel: {}}
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), labels[:])
+    return myTree
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else:
+                classLabel = secondDict[key]
+    return classLabel
+
+def storeTree(inputTree, filename):
+    import pickle
+    fw = open(filename, 'w')
+    pickle.dump(inputTree, fw)
+    fw.close()
+
+def grabTree(filename):
+    import pickle
+    fr = open(filename)
+    return pickle.load(fr)
