9c45bac9e1d092d51316796c389369e3837dbe3f
liaowei02 <liaowei02@baidu.com>
2017-06-28 19:52:29 +0800

    update

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -33,11 +33,15 @@ def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numVocab = len(trainMatrix[0])
     pAbusive = sum(trainCategory) / float(numTrainDocs)
-    # 创建长度为numVocab，值全部为0的数组
-    p0Num = np.zeros(numVocab)
-    p1Num = np.zeros(numVocab)
-    p0Denom = 0.0
-    p1Denom = 0.0
+    # 为了保证概率不为0，所以这里不能用0初始化
+    # p0Num = np.zeros(numVocab)
+    # p1Num = np.zeros(numVocab)
+    # p0Denom = 0.0
+    # p1Denom = 0.0
+    p0Num = np.ones(numVocab)   # 所有单词在分类为0的文档中出现的次数
+    p1Num = np.ones(numVocab)   # 所有单词在分类为1的文档中出现的次数
+    p0Denom = 2.0   # 
+    p1Denom = 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -45,6 +49,31 @@ def trainNB0(trainMatrix, trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num / p1Denom
-    p0Vect = p0Num / p0Denom
+    # 为了防止概率太小导致下溢出所以这里取对数
+    # p1Vect = p1Num / p1Denom
+    # p0Vect = p0Num / p0Denom
+    p1Vect = np.log(p1Num / p1Denom)
+    p0Vect = np.log(p0Num / p0Denom)
     return p0Vect, p1Vect, pAbusive
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClassl):
+    p1 = sum(vec2Classify * p1Vec) + np.log(pClassl)
+    p0 = sum(vec2Classify * p0Vec) + np.log(1 - pClassl)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    postList, classList = loadDataSet()
+    myVocabList = createVocabList(postList)
+    trainMat = []
+    for post in postList:
+        trainMat.append(setOfWords2Vec(myVocabList, post))
+    p0V, p1V, pAb = trainNB0(trainMat, classList)
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = setOfWords2Vec(myVocabList, testEntry)
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+    testEntry = ['stupid', 'garbage']
+    thisDoc = setOfWords2Vec(myVocabList, testEntry)
+    print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
--- ch04/test.py
+++ ch04/test.py
@@ -1,9 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import bayes
 
-postList, classList = bayes.loadDataSet()
-myVocabList = bayes.createVocabList(postList)
-trainMat = []
-for post in postList:
-    trainMat.append(bayes.setOfWords2Vec(myVocabList, post))
-p0Vec, p1Vec, pAb = bayes.trainNB0(trainMat, classList)
-print p0Vec, p1Vec, pAb
+bayes.testingNB()
