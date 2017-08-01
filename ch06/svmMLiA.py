b912ac28ca76db4866db2b1027bdb754aa4e601a
liaowei02 <liaowei02@baidu.com>
2017-08-01 15:07:24 +0800

    update

--- ch06/svmMLiA.py
+++ ch06/svmMLiA.py
@@ -37,5 +37,44 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
         for i in range(m):
             fXi = float(np.multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
             Ei = fXi - float(labelMat[i])
-
-        iter += 1
+            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i, m)
+                fXj = float(np.multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T)) + b
+                Ej = fXj - float(labelMat[j])
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                if (labelMat[i] != labelMat[j]):
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                if L == H:
+                    print('L == H')
+                    continue
+                eta = 2.0 * dataMatrix[i, :] * dataMatrix[j, :].T - dataMatrix[i, :] * dataMatrix[i, :].T - dataMatrix[j, :] * dataMatrix[j, :].T
+                if eta >= 0:
+                    print('eta >= 0')
+                    continue
+                alphas[j] -= labelMat[j] * (Ei - Ej) / eta
+                alphas[j] = clipAlpha(alphas[j], H, L)
+                if (abs(alphas[j] - alphaJold) < 0.00001):
+                    print('j not moving enough')
+                    continue
+                alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])
+                b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[i, :].T - labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[i, :] * dataMatrix[j, :].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, :] * dataMatrix[j, :].T - labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[j, :] * dataMatrix[j, :].T
+                if (0 < alphas[i]) and (C > alphas[i]):
+                    b = b1
+                elif (0 < alphas[j]) and (C > alphas[j]):
+                    b = b2
+                else:
+                    b = (b1 + b2) / 2.0
+                alphaPairsChanged += 1
+                print('iter: %d i: %d, pairs changed %d' % (iter, i, alphaPairsChanged))
+        if (alphaPairsChanged == 0):
+            iter += 1
+        else:
+            iter = 0
+        print('iteration number: %d' % iter)
+    return b, alphas
--- ch06/test.py
+++ ch06/test.py
@@ -3,5 +3,9 @@
 import svmMLiA
 
 dataArr, labelArr = svmMLiA.loadDataSet('testSet.txt')
-svmMLiA.smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
-
+b, alphas = svmMLiA.smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+print(b)
+print(alphas[alphas > 0])
+for i in range(100):
+    if alphas[i] > 0:
+        print(dataArr[i], labelArr[i])
