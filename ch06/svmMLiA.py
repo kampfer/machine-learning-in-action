e6153364f54451d7d389527035a1cd7e987b8c47
liaowei02 <liaowei02@baidu.com>
2017-08-02 19:48:26 +0800

    update

--- ch06/svmMLiA.py
+++ ch06/svmMLiA.py
@@ -43,6 +43,9 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 Ej = fXj - float(labelMat[j])
                 alphaIold = alphas[i].copy()
                 alphaJold = alphas[j].copy()
+                # SMO算法参考：
+                # http://blog.csdn.net/luoshixian099/article/details/51227754
+                # http://staff.ustc.edu.cn/~ketang/PPT/PRLec5.pdf
                 if (labelMat[i] != labelMat[j]):
                     L = max(0, alphas[j] - alphas[i])
                     H = min(C, C + alphas[j] - alphas[i])
@@ -78,3 +81,45 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
             iter = 0
         print('iteration number: %d' % iter)
     return b, alphas
+
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = np.shape(dataMatIn)[0]
+        self.alphas = np.mat(np.zeros((self.m, 1)))
+        self.b = 0
+        self.eCache = np.mat(zeros((self.m, 2)))
+
+def calcEk(oS, k):
+    fXk = float(np.multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k,:].T)) + oS.b
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+def selectJ(i, oS, Ei):
+    maxK = -1
+    maxDeltaE = 0
+    Ej = 0
+    oS.eCache[i] = [1, Ei]
+    validEcacheList = np.nonzero(oS.eCache[:, 0].A)[0]
+    if (len(validEcacheList)) > 1:
+        for k in validEcacheList:
+            if k == i:
+                continue
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei - Ek)
+            if (daltaE > maxDeltaE):
+                maxK = k
+                maxDeltaE = deltaE
+                Ej = Ek
+        return maxK, Ej
+    else:
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j, Ej
+
+def updateEk(oS, k):
+    Ek = calcEk(oS, k)
+    oS.eCache[k] = [1, Ek]
