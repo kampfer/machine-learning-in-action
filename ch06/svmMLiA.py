913e81e96781b7091bfc75c18646061a437174d7
l.w.kampfer <l.w.kampfer@gmail.com>
2017-08-06 15:57:16 +0800

    update

--- ch06/svmMLiA.py
+++ ch06/svmMLiA.py
@@ -91,7 +91,7 @@ class optStruct:
         self.m = np.shape(dataMatIn)[0]
         self.alphas = np.mat(np.zeros((self.m, 1)))
         self.b = 0
-        self.eCache = np.mat(zeros((self.m, 2)))
+        self.eCache = np.mat(np.zeros((self.m, 2)))
 
 def calcEk(oS, k):
     fXk = float(np.multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k,:].T)) + oS.b
@@ -110,7 +110,7 @@ def selectJ(i, oS, Ei):
                 continue
             Ek = calcEk(oS, k)
             deltaE = abs(Ei - Ek)
-            if (daltaE > maxDeltaE):
+            if (deltaE > maxDeltaE):
                 maxK = k
                 maxDeltaE = deltaE
                 Ej = Ek
@@ -123,3 +123,66 @@ def selectJ(i, oS, Ei):
 def updateEk(oS, k):
     Ek = calcEk(oS, k)
     oS.eCache[k] = [1, Ek]
+
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if (oS.labelMat[i] * Ei < -oS.tol and oS.alphas[i] < oS.C) or (oS.labelMat[i] * Ei > oS.tol and oS.alphas[i] > 0):
+        j, Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy()
+        alphaJold = oS.alphas[j].copy()
+        if (oS.labelMat[i] == oS.labelMat[j]):
+            L = max(0, oS.alphas[j] - oS.alphas[i])
+            H = min(oS.C, oS.C + oS.alphas[j] - oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j] + oS.alphas[i] - oS.C)
+            H = min(oS.C, oS.alphas[j] + oS.alphas[i])
+        if L == H:
+            print('L == H')
+            return 0
+        eta = 2.0 * oS.X[i, :] * oS.X[j, :].T - oS.X[i, :] * oS.X[i, :].T - oS.X[j, :] * oS.X[j, :].T
+        if eta >= 0:
+            print('eta >= 0')
+            return 0
+        oS.alphas[j] -= oS.labelMat[j] * (Ei - Ej) / eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001):
+            print('j not moving enough')
+            return 0
+        oS.alphas[i] += oS.labelMat[j] * oS.labelMat[i] * (alphaJold - oS.alphas[j])
+        b1 = oS.b - Ei - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.X[i, :] * oS.X[i, :].T - oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.X[i, :] * oS.X[j, :].T
+        b2 = oS.b - Ej - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * oS.X[i, :] * oS.X[j, :].T - oS.labelMat[j] * (oS.alphas[j] - alphaJold) * oS.X[j, :] * oS.X[j, :].T
+        if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]):
+            oS.b = b1
+        elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]):
+            oS.b = b2
+        else:
+            oS.b = (b1 + b2) / 2.0
+        return 1
+    else:
+        return 0
+
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+    oS = optStruct(np.mat(dataMatIn), np.mat(classLabels).transpose(), C, toler)
+    iter = 0
+    entrieSet = True
+    alphaPairsChanged = 0
+    while (iter < maxIter) and ((alphaPairsChanged > 0) or (entrieSet)):
+        alphaPairsChanged = 0
+        if entrieSet:
+            for i in range(oS.m):
+                alphaPairsChanged += innerL(i, oS)
+            print('fullSet, iter: %d i: %d, pairs changed %d' %(iter, i, alphaPairsChanged))
+            iter += 1
+        else:
+            nonBoundIs = np.nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i, oS)
+                print('non-bound, iter: %d i: %d, pairs changed: %d' % (iter, i, alphaPairsChanged))
+            iter += 1
+        if entrieSet:
+            entrieSet = False
+        elif (alphaPairsChanged == 0):
+            entrieSet = True
+        print('iteration number: %d' % iter)
+    return oS.b, oS.alphas
--- ch06/test.py
+++ ch06/test.py
@@ -3,7 +3,8 @@
 import svmMLiA
 
 dataArr, labelArr = svmMLiA.loadDataSet('testSet.txt')
-b, alphas = svmMLiA.smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+# b, alphas = svmMLiA.smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+b, alphas = svmMLiA.smoP(dataArr, labelArr, 0.6, 0.001, 40)
 print(b)
 print(alphas[alphas > 0])
 for i in range(100):
