65fa8ddcfa475de3aefe2f87a993560b145ffbf1
liaowei02 <liaowei02@baidu.com>
2017-08-08 18:54:55 +0800

    update

--- ch07/adaboost.py
+++ ch07/adaboost.py
@@ -45,3 +45,37 @@ def buildStump(dataArr, classLabels, D):
                     bestStump['thresh'] = threshVal
                     bestStump['ineq'] = inequal
     return bestStump, minError, bestClassEst
+
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    weekClassArr = []
+    m = np.shape(dataArr)[0]
+    D = np.mat(np.ones((m,1))/m)
+    aggClassEst = np.mat(np.zeros((m,1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print('D:',D.T)
+        alpha = float(0.5 * np.log((1.0-error)/max(error,1e-16)))
+        bestStump['alpha'] = alpha
+        weekClassArr.append(bestStump)
+        print('classEst:', classEst.T)
+        expon = np.multiply(-1 * alpha * np.mat(classLabels).T, classEst)
+        D = np.multiply(D, np.exp(expon))
+        D = D / D.sum()
+        aggClassEst += alpha * classEst
+        print('aggClassEst: ', aggClassEst.T)
+        aggErrors = np.multiply(np.sign(aggClassEst) != np.mat(classLabels).T, np.ones((m,1)))
+        errorRate = aggErrors.sum() / m
+        print('total error: ', errorRate, '\n')
+        if errorRate == 0.0:
+            break
+    return weekClassArr
+
+def adaClassify(dataToClass, classifierArr):
+    dataMatrix = np.mat(dataToClass)
+    m = np.shape(dataMatrix)[0]
+    aggClassEst = np.mat(np.zeros((m,1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], classifierArr[i]['thresh'], classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+        print aggClassEst
+    return np.sign(aggClassEst)
--- ch07/test.py
+++ ch07/test.py
@@ -3,7 +3,6 @@
 import adaboost
 import numpy as np
 
-datMat, classLabels = adaboost.loadSimpData()
-D = np.mat(np.ones((5,1)) / 5)
-bestStump, minError, bestClassEst = adaboost.buildStump(datMat, classLabels, D)
-print(bestStump, minError, bestClassEst)
+dataArr, classLabels = adaboost.loadSimpData()
+classifierArr = adaboost.adaBoostTrainDS(dataArr, classLabels, 30)
+print(adaboost.adaClassify([0,0], classifierArr))
