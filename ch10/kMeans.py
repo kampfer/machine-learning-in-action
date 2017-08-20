bf3c3ddf460a6ea9117e994df6b1bf9be10cd5fc
l.w.kampfer <l.w.kampfer@gmail.com>
2017-08-20 22:29:53 +0800

    update

--- ch10/kMeans.py
+++ ch10/kMeans.py
@@ -14,11 +14,67 @@ def loadDataSet(fileName):
 def distEclud(vecA, vecB):
     return np.sqrt(np.sum(np.power(vecA - vecB, 2)))
 
+# 为给定数据集构建一个包含k个随机质心的集合
 def randCent(dataSet, k):
     n = np.shape(dataSet)[1]
     centroids = np.mat(np.zeros((k, n)))
     for j in range(n):
         minJ = np.min(dataSet[:,j])
-        rangeJ = float(np.max(dataSet[:,j]) - minJ)
+        maxJ = np.max(dataSet[:,j])
+        rangeJ = float(maxJ - minJ)
         centroids[:,j] = np.mat(minJ + rangeJ * np.random.rand(k,1))
     return centroids
+
+# 簇的数目是由用户预先定义的
+# 默认通过随机算法得到质心
+def kMeans(dataSet, k, disMeas=distEclud, createCent=randCent):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDist = np.inf
+            minIndex = -1
+            for j in range(k):
+                distJI = disMeas(centroids[j,:], dataSet[i,:])
+                if distJI < minDist:
+                    minDist = distJI
+                    minIndex = j
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+            clusterAssment[i,:] = minIndex, minDist**2
+        # print(centroids)
+        for cent in range(k):
+            ptsInClust = dataSet[np.nonzero(clusterAssment[:,0].A == cent)[0]]
+            centroids[cent,:] = np.mean(ptsInClust, axis=0)
+    return centroids, clusterAssment
+
+def biKmeans(dataSet, k, disMeas=distEclud):
+    m = np.shape(dataSet)[0]
+    clusterAssment = np.mat(np.zeros((m, 2)))
+    centroid0 = np.mean(dataSet, axis=0).tolist()[0]
+    # print(centroid0)
+    centList = [centroid0]
+    for j in range(m):
+        clusterAssment[j,1] = disMeas(np.mat(centroid0), dataSet[j,:])**2
+    while (len(centList) < k):
+        lowestSSE = np.inf
+        for i in range(len(centList)):
+            ptsInCurrCluster = dataSet[np.nonzero(clusterAssment[:,0].A == i)[0],:]
+            centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, disMeas)
+            sseSplit = np.sum(splitClustAss[:,1])
+            sseNotSplit = np.sum(clusterAssment[np.nonzero(clusterAssment[:,0].A != i)[0],1])
+            print('sseSplit: %f, sseNotSplit: %f'%(sseSplit, sseNotSplit))
+            if (sseSplit + sseNotSplit) < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClustAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        bestClustAss[np.nonzero(bestClustAss[:,0].A == 1)[0], 0] = len(centList)
+        bestClustAss[np.nonzero(bestClustAss[:,0].A == 0)[0], 0] = bestCentToSplit
+        centList[bestCentToSplit] = bestNewCents[0,:]
+        centList.append(bestNewCents[1,:])
+        clusterAssment[np.nonzero(clusterAssment[:,0].A == bestCentToSplit)[0],:] = bestClustAss
+    return np.mat(centList), clusterAssment
--- ch10/test.py
+++ ch10/test.py
@@ -8,4 +8,18 @@ def test1():
     print kMeans.randCent(dataMat, 2)
     print kMeans.distEclud(dataMat[0], dataMat[1])
 
-test1()
+def test2():
+    dataMat = np.mat(kMeans.loadDataSet('testSet.txt'))
+    myCentroids, clusterAssing = kMeans.kMeans(dataMat, 4)
+    print(clusterAssing)
+
+def test3():
+    dataMat = np.mat(kMeans.loadDataSet('testSet.txt'))
+    kMeans.biKmeans(dataMat, 4)
+
+def test4():
+    dataMat = np.mat(kMeans.loadDataSet('testSet2.txt'))
+    centList, myNewAssments = kMeans.biKmeans(dataMat, 3)
+    print centList
+
+test4()
