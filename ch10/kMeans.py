d405e29180b763b3a55a03f111344cf2ca619ae0
liaowei02 <liaowei02@baidu.com>
2017-08-23 11:34:03 +0800

    update

--- ch10/kMeans.py
+++ ch10/kMeans.py
@@ -89,3 +89,33 @@ def plotScatter(dataMat, centList, clusterAssment):
     axs.scatter(dataMat[:,0].A, dataMat[:,1].A)
     axs.scatter(centList[:,0].A, centList[:,1].A, c='red')
     plt.show()
+
+import urllib
+import json
+def geoGrab(stAddress, city):
+    apiStem = 'http://where.yahooapis.com/geocode?'  #create a dict and constants for the goecoder
+    params = {}
+    params['flags'] = 'J'#JSON return type
+    params['appid'] = 'aaa0VN6k'
+    params['location'] = '%s %s' % (stAddress, city)
+    url_params = urllib.urlencode(params)
+    yahooApi = apiStem + url_params      #print url_params
+    print yahooApi
+    c=urllib.urlopen(yahooApi)
+    return json.loads(c.read())
+
+from time import sleep
+def massPlaceFind(fileName):
+    fw = open('places.txt', 'w')
+    for line in open(fileName).readlines():
+        line = line.strip()
+        lineArr = line.split('\t')
+        retDict = geoGrab(lineArr[1], lineArr[2])
+        if retDict['ResultSet']['Error'] == 0:
+            lat = float(retDict['ResultSet']['Results'][0]['latitude'])
+            lng = float(retDict['ResultSet']['Results'][0]['longitude'])
+            print "%s\t%f\t%f" % (lineArr[0], lat, lng)
+            fw.write('%s\t%f\t%f\n' % (line, lat, lng))
+        else: print "error fetching"
+        sleep(1)
+    fw.close()
