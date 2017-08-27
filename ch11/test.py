043d84e037ba80a75af657665c83be028fc59603
l.w.kampfer <l.w.kampfer@gmail.com>
2017-08-27 17:00:05 +0800

    update

--- ch11/test.py
+++ ch11/test.py
@@ -5,7 +5,8 @@ import apriori
 def test1():
     dataSet = apriori.loadDataSet()
     C1 = apriori.createC1(dataSet)
-    L, supportData = apriori.apriori(dataSet)
-    print(L)
+    L, supportData = apriori.apriori(dataSet, minSupport=0.5)
+    rules = apriori.generateRules(L, supportData)
+    print(rules)
 
 test1()
