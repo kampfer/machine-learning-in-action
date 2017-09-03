6e5c0a69bf1804ca232b841d944b6812bf2a0ceb
l.w.kampfer <l.w.kampfer@gmail.com>
2017-09-03 15:37:55 +0800

    update ch13

--- ch13/pca.py
+++ ch13/pca.py
@@ -7,3 +7,15 @@ def loadDataSet(fileName, delim='\t'):
     stringArr = [line.strip().split(delim) for line in fr.readlines()]
     dataArr = [map(float, line) for line in stringArr]
     return np.mat(dataArr)
+
+def pca(dataMat, topNfeat=9999999):
+    meanVals = np.mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals
+    covMat = np.cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = np.linalg.eig(covMat)
+    eigValInd = np.argsort(eigVals)
+    eigValInd = eigValInd[:-(topNfeat + 1):-1]
+    redEigVects =  eigVects[:,eigValInd]
+    lowDDataMat = meanRemoved * redEigVects
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat, reconMat
