5f571c5b894c327e662a7e9f3c46016ef5574517
l.w.kampfer <l.w.kampfer@gmail.com>
2017-09-03 14:58:58 +0800

    add ch13

---
+++ ch13/pca.py
@@ -0,0 +1,9 @@
+# -*- utf-8 -*-
+
+import numpy as np
+
+def loadDataSet(fileName, delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
+    dataArr = [map(float, line) for line in stringArr]
+    return np.mat(dataArr)
