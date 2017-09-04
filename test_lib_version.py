8117a95cc29068e8fd08328b16734e0986a22770
liaowei02 <liaowei02@baidu.com>
2017-09-04 19:46:20 +0800

    增加常用库的版本检查

---
+++ test_lib_version.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import scipy
+import numpy
+import matplotlib
+import pandas
+
+print 'scipy version: %s'%scipy.__version__
+print 'numpy version: %s'%numpy.__version__
+print 'matplotlib version: %s'%matplotlib.__version__
+print 'pandas version: %s'%pandas.__version__
